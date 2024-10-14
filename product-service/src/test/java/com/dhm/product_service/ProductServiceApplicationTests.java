b1ca8bbd9294d6e35c4953a319cdfc5f8d415b59
Aris Bello M <arisbellomartinez@gmail.com>
2024-10-13 20:32:05 -0500

    avanzado

---
+++ product-service/src/test/java/com/dhm/product_service/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhm.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
